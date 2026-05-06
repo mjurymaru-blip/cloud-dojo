49c7d66c62f818564f156afb3c32786b2a05b0e3
h.oota <mjury.maru@gmail.com>
2026-05-06 21:12:54 +0900

    fix: disable PWA navigateFallback to fix app.html routing

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,9 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      workbox: {
+        navigateFallback: null
+      },
       manifest: {
         name: 'Cloud Dojo',
         short_name: 'Cloud Dojo',
